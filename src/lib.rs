9fab06954e4b5fe18d56e571b4f7c408a258032f
jyuch <jyuch@users.noreply.github.com>
2022-10-12 21:42:47 +0900

    Generic

--- src/lib.rs
+++ src/lib.rs
@@ -2,26 +2,26 @@ use crate::Cell::{Cons, Nil};
 use std::mem::replace;
 
 #[derive(Debug)]
-enum Cell {
+enum Cell<T> {
     Nil,
-    Cons(i32, Box<Cell>),
+    Cons(T, Box<Cell<T>>),
 }
 
-pub struct LinkedList {
-    head: Cell,
+pub struct LinkedList<T> {
+    head: Cell<T>,
 }
 
-impl LinkedList {
+impl<T> LinkedList<T> {
     pub fn new() -> Self {
         LinkedList { head: Nil }
     }
 
-    pub fn push(&mut self, value: i32) {
+    pub fn push(&mut self, value: T) {
         let next = Cons(value, Box::new(replace(&mut self.head, Nil)));
         self.head = next;
     }
 
-    pub fn pop(&mut self) -> Option<i32> {
+    pub fn pop(&mut self) -> Option<T> {
         match replace(&mut self.head, Nil) {
             Nil => None,
             Cons(v, n) => {
--- tests/push_and_pop.rs
+++ tests/push_and_pop.rs
@@ -1,7 +1,7 @@
 use linkedlist::LinkedList;
 
 #[test]
-fn can_push_and_pop_items() {
+fn can_push_and_pop_i32_items() {
     let mut list = LinkedList::new();
 
     list.push(1);
@@ -11,4 +11,19 @@ fn can_push_and_pop_items() {
     assert_eq!(Some(3), list.pop());
     assert_eq!(Some(2), list.pop());
     assert_eq!(Some(1), list.pop());
+    assert_eq!(None, list.pop());
+}
+
+#[test]
+fn can_push_and_pop_string_items() {
+    let mut list = LinkedList::new();
+
+    list.push("1");
+    list.push("2");
+    list.push("3");
+
+    assert_eq!(Some("3"), list.pop());
+    assert_eq!(Some("2"), list.pop());
+    assert_eq!(Some("1"), list.pop());
+    assert_eq!(None, list.pop());
 }
